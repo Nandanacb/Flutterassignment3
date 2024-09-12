4566dcc920e800f64d147df7353a5a089da3763c
Nandanacb <nandhnacb@gmail.com>
2024-09-12 20:00:53 +0530

    assignment 12/9/24

--- coffee1/lib/restaurands.dart
+++ coffee1/lib/restaurands.dart
@@ -11,64 +11,64 @@ class Restaurands extends StatelessWidget{
       body:Column(
         children: [
 
-          ClipRRect(
-           child: Stack(
-            children: [
-            
-            Container(
-            height: 300,
-            width: double.infinity,
-            decoration: BoxDecoration(image: DecorationImage(image: AssetImage("assets/images/download.jpeg"),
-            fit: BoxFit.cover),
-            ),
-          ),
-          Positioned(
-            top:10,
-            left:10,
-            child:Container(
-              height: 50,
-              width: 50,
-              child: Icon(Icons.arrow_back,color: Colors.white,size: 40, 
-            ), ),
-          ),
+          Stack(
+           children: [
            
-
-            Positioned(
-            top:10,
-            right:10,
-            child:Container(
-              height: 100,
-              width: 100,
-              child: Text("Filters",style: TextStyle(fontSize: 25,fontWeight:FontWeight.bold,color: Colors.white),
-            ), ),
-          ),
-           
-
+           ClipRRect(
+             child: Container(
+             height: 300,
+             width: double.infinity,
+             decoration: BoxDecoration(image: DecorationImage(image: AssetImage("assets/images/download.jpeg"),
+             fit: BoxFit.cover),
+             ),
+                      ),
+           ),
+                    Positioned(
+           top:10,
+           left:10,
+           child:Container(
+             height: 50,
+             width: 50,
+             child: Icon(Icons.arrow_back,color: Colors.white,size: 40, 
+           ), ),
+                    ),
+          
+          
            Positioned(
-            top:130,
-            left:10,
-            child:Container(
-              height: 200,
-              width: 230,
-              child:   Text("American Restaurants",style: TextStyle(fontSize: 40,fontWeight:FontWeight.bold,color: Colors.white),
-            ), ),
-          ),
-
-
-
+           top:10,
+           right:10,
+           child:Container(
+             height: 100,
+             width: 100,
+             child: Text("Filters",style: TextStyle(fontSize: 25,fontWeight:FontWeight.bold,color: Colors.white),
+           ), ),
+                    ),
+          
+          
           Positioned(
-            top:250,
-            left:10,
-            child:Container(
-              height: 250,
-              width: 260,
-              child:   Text("36 places",style: TextStyle(fontSize: 25,color: Colors.white),
-            ), ),
-          ),
-
-         ],
-          ),
-          ),
+           top:130,
+           left:10,
+           child:Container(
+             height: 200,
+             width: 230,
+             child:   Text("American Restaurants",style: TextStyle(fontSize: 40,fontWeight:FontWeight.bold,color: Colors.white),
+           ), ),
+                    ),
+          
+          
+          
+                    Positioned(
+           top:250,
+           left:10,
+           child:Container(
+             height: 250,
+             width: 260,
+             child:   Text("36 places",style: TextStyle(fontSize: 25,color: Colors.white),
+           ), ),
+                    ),
+          
+                   ],
+                    ),
           
           
           
@@ -96,7 +96,7 @@ class Restaurands extends StatelessWidget{
                     children: [
                       Container(
                         height: 160,
-                        width: 120,
+                        width: 140,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10),
                           image: DecorationImage(
@@ -111,7 +111,7 @@ class Restaurands extends StatelessWidget{
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                      
+                         
                           Text(Database.myrestaurantsList[index]['name'],
                           
                           style: TextStyle(
@@ -124,7 +124,7 @@ class Restaurands extends StatelessWidget{
                        SizedBox(height: 5),
                          Row(
                             children: [
-                          Icon(Icons.map,color: Colors.white),
+                          Icon(Icons.pin_drop,size:13,color:  Colors.white),
                           Text(Database.myrestaurantsList[index]['name1'],
                           
                           style: TextStyle(
@@ -137,50 +137,49 @@ class Restaurands extends StatelessWidget{
                         
                         ],),
 
-                          SizedBox(height: 19),
+                          SizedBox(height: 22),
                           
                           Row(
                             children: [
                               Icon(Icons.star,color: Colors.yellow,size: 10),
                               Text(Database.myrestaurantsList[index]['text1'],
                               style: TextStyle(color: Colors.white,fontSize: 9.5,fontWeight: FontWeight.bold),),
-
+                              
+                              
+                          
                             ],
                           ),
                         ],
                       ),
+
                      
-                      /*Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column( 
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Icon(Icons.favorite,size: 15,color: Colors.white),
-                            
-                          
-                          ],
-                          
-                          
-                        ),
-                      ),*/
                       
+                            
                           ],
+                        
                         ),
-                      ),
+              ),
+                     
+                      
+                      
+      );
+  },
+  
+      )],
+                        ),
+                      );
       
             
 
-              );
+              
             }
-            ),      
+              
   
-  ]
-      ),
+  
                 
-  );
+
   }
 
               
       
-  }
+  
